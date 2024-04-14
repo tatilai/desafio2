3588c8fb01e46d3435f310ae8158e5829531daaa
tatilai <99092149+tatilai@users.noreply.github.com>
2024-04-14 19:20:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener ("DOMContentLoaded",function(){
-    const xhr=
-})
+    const xhr= new XMLHttpRequest();
+    xhr.onreadystatechange=()=>{
+        if (xhr.readyState===4 && xhr.status===200){
+            const data =JSON.parse(xhr.responseText);
+            displayUsers(data);
+        }
+    };
+
+    
+});
